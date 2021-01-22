5b2436e7deae95ae6f79dc86d22312b8d844dee8
Lucien Greathouse <me@lpghatguy.com>
2021-01-22 13:04:26 -0700

    Do deployments still work? Let's find out!

--- src/packages/Dbs/Components/TripInterface.lua
+++ src/packages/Dbs/Components/TripInterface.lua
@@ -11,9 +11,15 @@ local function TripInterface(props)
 		end
 
 		return Roact.createElement("TextLabel", {
-			Size = UDim2.new(0, 40, 0, 40),
-			Position = UDim2.new(0, 80, 0, 80),
-			Text = tripStatus.type .. " : " .. tripStatus.progress,
+			Size = UDim2.new(0, 400, 0, 57),
+			Position = UDim2.new(0.5, 0, 0, 80),
+			AnchorPoint = Vector2.new(0.5, 0),
+			BackgroundColor3 = Color3.fromRGB(12, 12, 12),
+			Color3 = Color3.fromRGB(240, 240, 240),
+			BorderSizePixel = 0,
+			TextSize = 40,
+			Font = Enum.Font.SourceSans,
+			Text = tripStatus.type .. " : " .. math.round(tripStatus.progress),
 		})
 	end)
 end
